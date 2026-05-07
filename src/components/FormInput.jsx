84f0837be72280e56300ea9a0c74859e2d293286
unknown <edvan.aoliveira324@gmail.com>
2026-05-07 17:23:06 -0300

    feat: finaliza campos estaticos para formulario de novo projeto

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,19 +1,25 @@
+import { Icon } from "lucide-react";
+
 function FormInput(
     {
-        className="",
-        label,
-        placeholder
+        textRows,
+        inputClassName = "text-base",
+        labelContent,
+        placeholder,
+        icon
     }
 ) {
     return (
         <div className="flex flex-col">
-            <label>{label}</label>
-            <div className="border-[1px] rounded-xl px-4 py-3 text-(--color-cinza-700)">
-                <input
-                    className="focus:outline-0"
+            <label className="font-inter font-medium text-lg text-(--cinza-700)">{labelContent}</label>
+            <div className= {`flex justify-between border-[1px] rounded-xl px-4 py-3 ${className}`}>
+                <textarea
+                    rows={1}
+                    className={`flex wrap focus:outline-0 font-inter font-medium w-full ${inputClassName}`}
                     type="text"
                     placeholder={placeholder}
                 />
+                {icon}
             </div>
         </div>
     )
--- src/pages/project-creator/ProjectCreator.jsx
+++ src/pages/project-creator/ProjectCreator.jsx
@@ -3,12 +3,16 @@ import Title2 from "../../components/Typography/Title2.jsx";
 import FormInput from "../../components/FormInput.jsx";
 
 function ProjectCreator() {
+
+
     return (
         <div>
             <MobileHeader></MobileHeader>
             <div className="flex flex-col px-4 py-[10px] gap-3">
                 <Title2>Novo Projeto</Title2>
-                <FormInput label = "Titulo do Projeto" placeholder = "Novo Projeto"></FormInput>
+                <FormInput labelContent = "Titulo do Projeto" inputClassName="text-lg" placeholder = "Novo Projeto"></FormInput>
+                <FormInput labelContent = "Sobre o projeto" placeholder = "Insira uma breve descrição sobre o projeto" className = "h-24"></FormInput>
+                <FormInput labelContent = "Integrantes" placeholder = "Buscar por email" icon={<Search  className ="text-(--cinza-700)"/>}> <Search></Search></FormInput>
 
             </div>
         </div>
