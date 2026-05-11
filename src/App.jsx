39246fe4fdcc0b5f7d32b5841a76c4f622ca9ee4
Samara Rolim dos Santos <samara_rolim010@hotmail.com>
2026-05-11 11:19:43 -0300

    Update App.jsx
    Desfazendo alterações no App

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,21 @@
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Login from "./pages/login/Login";
-// import Cadastro from "./pages/login/Cadastro";
-// import Senha from "./pages/login/Senha";
-// import Redefinir from "./pages/login/Redefinir";
-// import Dashboard from "./pages/Dashboard";
-import ProjectDetails from "./pages/project-details/ProjectDetails";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/login/Login";
+import Cadastro from "./pages/login/Cadastro";
+import Senha from "./pages/login/Senha";
+import Redefinir from "./pages/login/Redefinir";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <div>
-      <ProjectDetails />
-    </div>
-    //   <Router>
-    //     <Routes>
-    //       <Route path="/" element={<Login />} />
-    //       <Route path="/cadastro" element={<Cadastro />} />
-    //       <Route path="/Senha" element={<Senha />} />
-    //       <Route path="/Login" element={<Login />} />
-    //       <Route path="/Redefinir" element={<Redefinir />} />
-    //       <Route path="/dashboard" element={<Dashboard />} />
-    //     </Routes>
-    //   </Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/Senha" element={<Senha />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Redefinir" element={<Redefinir />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
