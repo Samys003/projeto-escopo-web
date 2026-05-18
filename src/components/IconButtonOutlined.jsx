3a471a76fe319b06fd22df1b505e0651376325a3
Edvan Alves <edvan.aoliveira324@gmail.com>
2026-05-18 00:07:08 -0300

    style: Adições de responsividade dashboard

--- src/components/IconButtonOutlined.jsx
+++ src/components/IconButtonOutlined.jsx
@@ -1,7 +1,10 @@
 //TODO: Refatorar IconButton para incluir esse como uma variação
 function IconButtonOutlined({ icon, onClick }) {
     return (
-        <button onClick={onClick} className=" border-(--cinza-300) border p-0 rounded-md">
+        <button
+            onClick={onClick}
+            className="cursor-pointer border-(--cinza-300) border p-0 rounded-md"
+        >
             {icon}
         </button>
     );
